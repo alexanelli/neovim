3c92ee625cc1475e3a579aaacf2dc80786f2e8aa
Alex Anelli <atanelli@protonmail.com>
2025-01-24 11:27:13 -0500

    change noice ui config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,4 +1,5 @@
 -- ui overhaul
+-- notifications/messages, cmdline popup
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
@@ -8,9 +9,17 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
+    { 'rcarriga/nvim-notify',
+      opts = {
+        timeout = 3000,
+        top_down = false,
+      }
+    },
   },
   opts = {
+    format = {
+      conceal = false
+    },
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
@@ -21,7 +30,7 @@ return {
     },
     -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
+      bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
