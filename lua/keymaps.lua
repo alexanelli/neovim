e23926f87f5e08719215ebb259dba87357d4b09a
Alex Anelli <atanelli@protonmail.com>
2026-01-05 16:03:28 -0500

    get rid of wezterm smart splits for now, window ctrl keybinds not working

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,10 +31,10 @@ vim.keymap.set('n', '<leader>iml', insert_markdown_list, { desc = '[i]nsert [m]a
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
 
 
 -- Clear highlights on search when pressing <Esc> in normal mode
