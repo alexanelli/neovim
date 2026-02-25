42d4efb5684be3e991a89e1851dbcf7c80c87741
Alex Anelli <atanelli@protonmail.com>
2026-02-25 16:53:40 -0500

    get lsp working with vim api

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -177,8 +177,11 @@ return {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- toggle to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- toggle to ignore Lua_LS's noisy `missing-fields` warnings
+                disable = { 'missing-fields' },
+                globals = { 'vim' },
+              },
             },
           },
         },
